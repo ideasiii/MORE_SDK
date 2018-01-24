2dbf448c7ccd02a1549247f9595e95d813f3ef15
ideasiii <louisju@iii.org.tw>
2018-01-24 16:57:38 +0800

    jugo update

---
+++ RestClient/src/main/java/org/iii/moresdk/restclient/RestClient.java
@@ -0,0 +1,13 @@
+package org.iii.moresdk.restclient;
+
+public class RestClient
+{
+    public RestClient()
+    {
+    }
+    
+    public String toString()
+    {
+        return "RestClient";
+    }
+}
